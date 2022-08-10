d157ab38c203524b449c085a87e81d88384fa936
0508-mj <mukamijoan00@gmail.com>
2022-08-10 04:44:36 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -2,7 +2,7 @@
 /**
  * _printf - is a function that selects the correct function to print.
  * @format: identifier to look for.
- * Return: the length of the string.
+ * Return: the length of the string..
  */
 int _printf(const char * const format, ...)
 {
