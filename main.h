f302eacc9f47fc2916e0f09dfe7b9d3d8d76b3c6
Mukami <mukamijoan00@gmail.com>
2022-08-10 19:19:04 +0300

    Update main.h

--- main.h
+++ main.h
@@ -1,49 +1,48 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdarg.h>
-#include <limits.h>
-#include <unistd.h>
-
-
+#include "main.h"
 
 /**
- * struct format - match the conversion specifiers for printf
- * @id: type char pointer of the specifier i.e (l, h) for (d, i, u, o, x, X)
- * @f: type pointer to function for the conversion specifier
- *
- */
+  *_printf - displays to the stdout according to a format
+  *@format: format string containg the characters and specifiers
+  *Description: This function calls get_print(). get_priint() function
+  *determines what to print depending on the format
+  *specifiers contained in @fmt
+  *Return: length of the formatted output string.
+  */
 
-typedef struct format
+int _printf(const char *format, ...)
 {
-	char *id;
-	int (*f)();
-} convert_match;
+	int (*pfunc)(va_list, flags_t *);
+	const char *p;
+	va_list args;
+	flags_t flags = {0, 0, 0};
 
-int printf_pointer(va_list val);
-int printf_hex_aux(unsigned long int num);
-int printf_HEX_aux(unsigned int num);
-int printf_exclusive_string(va_list val);
-int printf_HEX(va_list val);
-int printf_hex(va_list val);
-int printf_oct(va_list val);
-int printf_unsigned(va_list args);
-int printf_bin(va_list val);
-int printf_srev(va_list args);
-int printf_rot13(va_list args);
-int printf_int(va_list args);
-int printf_dec(va_list args);
-int _strlen(char *s);
-int *_strcpy(char *dest, char *src);
-int _strlenc(const char *s);
-int rev_string(char *s);
-int _strlenc(const char *s);
-int printf_37(void);
-int printf_char(va_list val);
-int printf_string(va_list val);
-int _putchar(char c);
-int _printf(const char *format, ...);
+	register int count = 0;
 
-#endif
+	va_start(args, format);
+	if (!format || (format[0] == '%' && !format[1]))
+		return (-1);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
+	for (p = format; *p; p++)
+	{
+		if (*p == '%')
+		{
+			p++;
+			if (*p == '%')
+			{
+				count += _putchar('%');
+				continue;
+			}
+			while (get_flag(*p, &flags))
+				p++;
+			pfunc = get_print(*p);
+			count += (pfunc)
+				? pfunc(args, &flags)
+				: _printf("%%%c", *p);
+		} else
+			count += _putchar(*p);
+	}
+	_putchar(-1);
+	va_end(args);
+	return (count);
+}
